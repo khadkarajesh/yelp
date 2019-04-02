585b0d85870abe60b8fb0103e25a27dfbfffaff9
Rajesh <rajesh.k.khadka@gmail.com>
2019-04-02 07:32:17 +0545

    add body parser

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
-var express = require('express');
-var app = express();
+var express = require('express')
+var bodyparser = require('body-parser')
+
+
+var app = express()
+app.use(bodyparser.json())
+
+
 app.get('/', function (req, res) {
-    res.send('Hello World!');
+    res.send('Hello World!')
 });
+
+app.post("/businesses", function(req, res){
+    console.log(req.body)
+    res.send("hi you there")
+})
 app.listen(3000, function () {
-    console.log('Hello Yelper');
+    console.log('Hello Yelper')
 });
