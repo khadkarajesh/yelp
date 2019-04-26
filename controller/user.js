d082576cfc5f97e766c028979e28049a0af896c1
Rajesh <rajesh.k.khadka@gmail.com>
2019-04-26 21:48:45 +0545

    issue tokens

--- controller/user.js
+++ controller/user.js
@@ -1,5 +1,6 @@
 var User = require('../db/models/user')
 const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 
 
 exports.create = function (req, res) {
@@ -9,19 +10,34 @@ exports.create = function (req, res) {
         }
 
         bcrypt.genSalt(10, function (err, salt) {
-            bcrypt.hash(req.body.password, salt, function (error, hash) {
-                var newUser = new User({
-                    first_name: req.body.first_name,
-                    middle_name: req.body.middle_name,
-                    last_name: req.body.last_name,
-                    email:req.body.email,
-                    password: hash
+            bcrypt.hash(req.body.password,
+                salt,
+                function (error, hash) {
+                    var newUser = new User({
+                        first_name: req.body.first_name,
+                        middle_name: req.body.middle_name,
+                        last_name: req.body.last_name,
+                        email: req.body.email,
+                        password: hash
+                    })
+
+                    newUser.save(function (err) {
+                        if (err) return res.json(err)
+                        var accessToken = jwt.sign({ email: newUser.email }, process.env.APP_SECRET_KEY, { expiresIn: 60 * 60 })
+                        var refreshToken = jwt.sign({ email: newUser.email }, process.env.APP_SECRET_KEY, { expiresIn: 24 * 60 * 60 })
+                        res.json({
+                            message: "success",
+                            accessToken: accessToken,
+                            refreshToken: refreshToken,
+                            data: {
+                                first_name: newUser.first_name,
+                                middle_name: newUser.middle_name,
+                                last_name: newUser.last_name,
+                                email: newUser.email
+                            }
+                        })
+                    })
                 })
-                newUser.save(function (err) {
-                    if (err) return res.json(err)
-                    res.json({ message: "success", data: newUser })
-                })
-            })
         })
     })
 }
