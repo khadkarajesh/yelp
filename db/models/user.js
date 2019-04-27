7171a1f1cda742e46a11115df0214ae2e964d659
Rajesh <rajesh.k.khadka@gmail.com>
2019-04-27 20:55:29 +0545

    remove validation from model

--- db/models/user.js
+++ db/models/user.js
@@ -5,14 +5,8 @@ var schema = mongoose.Schema({
     last_name: { type: String, required: true },
     email: { type: String, required: true },
     password: {
-        // type: String, required: true, validate: {
-        //     validator: function (v) {
-        //         return '/^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$/'.match(v)
-        //     },
-        //     message: 'Password must be length of min 6 must contain at least one number and special case'
-        // },
         type:String,
-        required:[true, 'Password is required']
+        required:true
     },
     refreshToken: [{ type: String }]
 })
