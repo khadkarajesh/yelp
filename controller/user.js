d6ccb806da9508539bf2af2ef1ebd321865f48c0
Rajesh <rajesh.k.khadka@gmail.com>
2019-05-03 11:36:44 +0000

    add middleware to authorized resource access

--- controller/user.js
+++ controller/user.js
@@ -2,15 +2,21 @@ var User = require('../db/models/user')
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
 const AppError = require('../helpers/AppError')
+
+module.exports = {
+    signup,
+    signin,
+}
+
 const PASSWORD_VALIDATION_REGEX = /^(?=.*[\d])(?=.*[A-Z])(?=.*[a-z])(?=.*[!@#$%^&*])[\w!@#$%^&*]{6,}$/
 const MSG_EMAIL_ALREADY_EXIST = 'This email is already registered'
 const MSG_INVALID_PASSWORD = 'Password must be length of min 6 must contain at least 1 number,1 lowercase, 1 capital letter, 1 special character'
 const ACCESS_TOKEN_EXPIRY_TIME = 60 * 60
 const REFRESH_TOKEN_EXPIRY_TIME = 24 * 60 * 60
-const MSG_INVALID_PASSWORD = 'Invalid username/password'
+const INVALID_PASSWORD = 'Invalid username/password'
 
 
-exports.signup = async function (req, res, next) {
+async function signup(req, res, next) {
     try {
         var user = await User.findOne({ email: req.body.email })
         if (user) throw new AppError(MSG_EMAIL_ALREADY_EXIST, 409)
@@ -39,27 +45,28 @@ exports.signup = async function (req, res, next) {
     }
 }
 
-exports.signin = async function (req, res, next) {
+async function signin(req, res, next) {
     try {
-        var salt = await bcrypt.genSalt(10)
-        var hash = await bcrypt.hash(req.body.password, salt)
-        var user = await User.findOne({ email: req.body.username, password: hash })
-        if (!user) throw new AppError(MSG_INVALID_PASSWORD, 401)
+        var user = await User.findOne({ email: req.body.username })
+        if (!user) throw new AppError(INVALID_PASSWORD, 401)
 
-        var accessToken = await jwt.sign({ email: user.email },
-            process.env.APP_SECRET_KEY,
-            { expiresIn: 60 * 60 })
-        var refreshToken = await jwt.sign({ email: user.email },
-            process.env.APP_SECRET_KEY,
-            { expiresIn: 24 * 60 * 60 })
-        user.refreshToken.push(refreshToken)
-        await user.save()
-        return res.json({
-            message: "success",
-            accessToken: accessToken,
-            refreshToken: refreshToken,
-            data: user
-        })
+        var result = await bcrypt.compare(req.body.password, user.password)
+        if (result) {
+            var accessToken = await jwt.sign({ email: user.email },
+                process.env.APP_SECRET_KEY,
+                { expiresIn: 60 * 60 })
+            var refreshToken = await jwt.sign({ email: user.email },
+                process.env.APP_SECRET_KEY,
+                { expiresIn: 24 * 60 * 60 })
+            user.refreshToken.push(refreshToken)
+            await user.save()
+            return res.json({
+                message: "success",
+                accessToken: accessToken,
+                refreshToken: refreshToken,
+                data: user
+            })
+        }
     } catch (error) {
         next(error)
     }
