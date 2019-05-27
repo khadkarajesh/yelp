7451c5194597d5a9bff6489fbd0db0ea6ea0a946
Rajesh <rajesh.k.khadka@gmail.com>
2019-05-27 21:39:42 +0545

    keep credential secret

--- helpers/facebookAuth.js
+++ helpers/facebookAuth.js
@@ -2,8 +2,8 @@ var Strategy = require('passport-facebook-token')
 const passport = require('passport')
 const User = require('../db/models/user')
 passport.use(new Strategy({
-    clientID: '2303355803286582',
-    clientSecret: 'a8d3be2a822806ac389c5aec5ea35782',
+    clientID: process.env.FACEBOOK_APP_ID,
+    clientSecret: process.env.FACEBOOK_APP_SECRET,
     fbGraphVersion: 'v3.0'
 }, async (accessToken, refreshToken, profile, done) => {
     console.log(profile)
